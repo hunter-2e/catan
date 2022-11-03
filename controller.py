841337d10b5249e42a61f1b427f8ec3124247e49
Kobi Feldman <kobif2001@gmail.com>
2022-11-02 20:21:52 -0400

    controller

--- controller.py
+++ controller.py
@@ -1,6 +1,6 @@
 def setup():
     """Handles all game setup."""
-    ...
+    pass
     # players are given a color, and their starting pieces
     # via some method, board is setup
     # players are randomly given a starting order (or with dice rolls)
@@ -9,7 +9,7 @@ def setup():
 
 def run():
     """Controls the main game loop."""
-    ...
+    pass
     # loop through each player until someone wins
     # player is forced to roll at the start of their turn, resource cards are automatically distributed
     # if a 7 is rolled handle robber stuff
@@ -19,4 +19,7 @@ def run():
 
 def game_over():
     """Handles any cleanup that needs to occur when a player wins the game."""
-    ...
+    pass
+
+if __name__ == "__main__":
+    main()
